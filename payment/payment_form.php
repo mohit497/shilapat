f3a95e491a954d45810430662f013d87e7dada72
mohit497 <mohit497@users.noreply.github.com>
2018-11-26 15:58:29 +0530

    Create payment_form.php
    this file will shown before the actual payment

---
+++ payment/payment_form.php
@@ -0,0 +1,24 @@
+<?php
+
+// include the config file
+
+include('./config.php');
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// insert into product table
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+?>
